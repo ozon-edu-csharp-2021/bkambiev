f3912eac0343529f7cd3c2a7e9ba3b8712b83ca5
kambievbeslan <kambiev.b.m@gmail.com>
2021-11-17 20:29:07 +0300

    fix

--- src/MerchandiseService.Domain/AggregationModels/EmployeeAggregate/Status.cs
+++ src/MerchandiseService.Domain/AggregationModels/EmployeeAggregate/Status.cs
@@ -1,14 +1,20 @@
 using MerchandiseService.Domain.Models;
+using System.Collections.Generic;
 
 namespace MerchandiseService.Domain.AggregationModels.EmployeeAggregate
 {
-    public class Status : Entity
+    public class Status : ValueObject
     {
         public StatusType Type { get; }
 
         public Status(StatusType type)
         {
             Type = type;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Type;
         }
     }
 }
