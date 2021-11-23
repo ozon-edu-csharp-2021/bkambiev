17804e629382097caa793168dfa949a870cd2fe1
kambievbeslan <kambiev.b.m@gmail.com>
2021-11-23 19:59:26 +0300

    Request начал

---
+++ src/MerchandiseService.Domain/AggregationModels/MerchPackRequest/MerchPackRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchandiseService.Domain.AggregationModels.MerchPackRequest
+{
+    internal class MerchPackRequest
+    {
+
+    }
+}
---
+++ src/MerchandiseService.Domain/AggregationModels/MerchPackRequest/MerchPackRequestStatus.cs
@@ -0,0 +1,29 @@
+﻿using MerchandiseService.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchandiseService.Domain.AggregationModels.MerchPackRequest
+{
+    
+    public class MerchPackRequestStatus : Enumeration
+    {
+
+        public static MerchPackRequestStatus New = new(1, "new");
+
+        public static MerchPackRequestStatus Processing = new(1, "processing");
+
+        public static MerchPackRequestStatus Done = new(1, "done");
+
+        public static MerchPackRequestStatus Decline = new(1, "decline");
+
+
+        public MerchPackRequestStatus(int id, string name) : base(id, name)
+        {
+
+        }
+
+    }
+}
